103b820ecc39155987fa110460697eb33ba02f88
IchiroKawashima <kawashima.ichiro172@mail.kyutech.jp>
2019-10-24 21:49:04 +0900

    add constraint kind for shifter

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Utils
     ( dfold'
@@ -9,15 +10,38 @@ import           Clash.Prelude
 import           Data.Proxy
 import           Data.Kind
 import           Data.Singletons.Prelude        ( TyFun
+                                                , Apply
                                                 , type (@@)
                                                 )
+import           Data.Constraint
+import           Data.Constraint.Nat
+
+data Shifter (k :: Nat) (f :: TyFun Nat Type) :: Type
+type instance Apply (Shifter k) l = (BitVector l, BitVector (2 ^ (k - l)))
+
+shifter :: forall k . (KnownNat k, 1 <= k) => BitVector (2 ^ k) -> BitVector k
+shifter x = fst $ dfold' (Proxy @(Shifter k)) sft (def, x) $ repeat ()
+  where
+    sft
+        :: forall l
+         . (KnownNat l, l <= k - 1)
+        => SNat l
+        -> ()
+        -> Shifter k @@ l
+        -> Shifter k @@ (l + 1)
+    sft SNat () (shift, remnant) =
+        case plusMonotone1 @l @(k - 1) @1 *** plusMinusInverse3 @1 @k of
+            Sub Dict
+                | bitToBool $ reduceOr lowerBits -> (shift ++# 0, lowerBits)
+                | otherwise                      -> (shift ++# 1, higherBits)
+                where (higherBits, lowerBits) = split remnant
 
 dfold'
     :: forall p k a
      . (KnownNat k)
     => Proxy (p :: TyFun Nat Type -> Type)
     -> (  forall l
-        . (KnownNat l, l <= k - 1, l + 1 <= k, l <= k)
+        . (KnownNat l, l <= k - 1)
        => SNat l
        -> a
        -> p @@ l
