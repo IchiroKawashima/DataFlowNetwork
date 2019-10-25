546ae434be8a9fbc5c34567491242f4139b0bd53
IchiroKawashima <kawashima.ichiro172@mail.kyutech.jp>
2019-10-25 19:16:40 +0900

    Add dtfold'

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE ExplicitNamespaces #-}
 
 module Utils
     ( dfold'
@@ -12,6 +13,8 @@ import           Data.Singletons.Prelude        ( TyFun
                                                 , Apply
                                                 , type (@@)
                                                 )
+import           Data.Constraint
+import           Data.Constraint.Nat
 
 dfold'
     :: forall p k a
@@ -33,3 +36,27 @@ dfold' Proxy f z = go
     go Nil           = z
     go (y `Cons` ys) = f (lengthS ys) y (go ys)
 {-# NOINLINE dfold' #-}
+
+dtfold'
+    :: forall p k a
+     . KnownNat k
+    => Proxy (p :: TyFun Nat Type -> Type)
+    -> (a -> p @@ 0)
+    -> (  forall l
+        . (KnownNat l, l <= k - 1)
+       => SNat l
+       -> p @@ l
+       -> p @@ l
+       -> p @@ (l + 1)
+       )
+    -> Vec (2 ^ k) a
+    -> (p @@ k)
+dtfold' _ f g = go
+  where
+    go :: forall n . (KnownNat n, n <= k) => Vec (2 ^ n) a -> p @@ n
+    go (x `Cons` Nil) = f x
+    go xs@(_ `Cons` (_ `Cons` _)) =
+        case leTrans @(n - 1) @n @(k - 1) *** leTrans @(n - 1) @n @k of
+            Sub Dict -> g (SNat @(n - 1)) (go xsl) (go xsr)
+                where (xsl, xsr) = splitAtI xs
+{-# NOINLINE dtfold' #-}
